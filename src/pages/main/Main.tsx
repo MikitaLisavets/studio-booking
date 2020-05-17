ff859f76ef472527fa9630b93059e41361b7a4cb
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-05-17 14:16:28 +0300

    Add plain locale selector

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,21 +2,36 @@ import './Main.scss';
 import React from 'react';
 import useShallowEqualSelector from '../../hooks/useShallowEqualSelector';
 import useAction from '../../hooks/useAction';
-import { getCounter } from '../../selectors';
+import { getCounter, getLocale } from '../../selectors';
 import * as Actions from '../../actions';
 import Header from '../../components/header/Header';
 import L from '../../utils/locale';
+import { useDispatch } from 'react-redux';
 
 export default function Main(): JSX.Element {
   const counter = useShallowEqualSelector(getCounter);
+  const locale = useShallowEqualSelector(getLocale);
+  const dispatch = useDispatch();
   const increment = useAction(Actions.increment());
   const decrement = useAction(Actions.decrement());
+  const setLocale = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    const value = event.target.value;
+    if (locale !== value) dispatch(Actions.setLocale(value));
+  };
+
 
   return (
     <div className="container">
       <Header/>
+      <div>Lang:</div>
+      <select onBlur={setLocale} onChange={setLocale}>
+        <option value="en" selected>En</option>
+        <option value="ru">Ru</option>
+      </select>
+
       <h1>{L('mainPage.title')}</h1>
       <div> Main {counter}</div>
+
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
