55b1548d9af5096452fbab25d7f1667e7e5f8803
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-06-09 18:35:24 +0300

    Add route constants

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -4,16 +4,17 @@ import Main from './pages/main/Main';
 import App from './pages/app/App';
 import SignUp from './pages/signup/SignUp';
 import Login from './pages/login/Login';
+import { SIGN_UP_ROUTE, MAIN_ROUTE, LOGIN_ROUTE } from './constants/navigation';
 
 export default function AppRouter(): JSX.Element {
   return (
     <div className="container">
       <Router>
         <Switch>
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={Login} />
+          <Route path={SIGN_UP_ROUTE} component={SignUp} />
+          <Route path={LOGIN_ROUTE} component={Login} />
           <Route path="/app" component={App} />
-          <Route path="/" component={Main} />
+          <Route path={MAIN_ROUTE} component={Main} />
         </Switch>
       </Router>
     </div>
