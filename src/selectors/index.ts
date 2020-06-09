4c6c7e4e0f3c512d9910ea38803581662d6eb235
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-06-09 18:39:36 +0300

    Add getUser selector

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,4 +1,5 @@
-import { AppState } from '../types/Types';
+import { AppState, User } from '../types/Types';
 
 export const getLocale = (state: AppState): string => state.locale;
 export const getCounter = (state: AppState): number => state.counter;
+export const getUser = (state: AppState): User | null => state.user;
