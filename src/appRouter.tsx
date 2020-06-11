0b87b8a3596523a79b3a2d8608d260cbbefbe657
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-06-11 18:26:38 +0300

    Use custom history

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router';
 import Main from './pages/main/Main';
 import SignUp from './pages/signup/SignUp';
 import Login from './pages/login/Login';
 import { SIGN_UP_ROUTE, MAIN_ROUTE, LOGIN_ROUTE } from './constants/navigation';
+import { createBrowserHistory } from 'history';
+
+
+export const history = createBrowserHistory();
 
 export default function AppRouter(): JSX.Element {
   return (
     <div className="container">
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route path={SIGN_UP_ROUTE} component={SignUp} />
           <Route path={LOGIN_ROUTE} component={Login} />
