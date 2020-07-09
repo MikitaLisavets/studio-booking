f311398bd8dc48cedeaca006cdd473cbc93ff136
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-07-09 21:57:16 +0300

    Add menu icon

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -1,4 +1,6 @@
 import { ReactComponent as AccountSvg } from './images/account.svg';
+import { ReactComponent as MenuSvg } from './images/menu.svg';
 import React from 'react';
 
 export const AccountIcon = (): JSX.Element => <AccountSvg className="icon-account"/>;
+export const MenuIcon = (): JSX.Element => <MenuSvg className="icon-menu"/>;
