4f59ee2a30451446c6f1072addbc3e37f1551e7d
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-07-09 22:11:26 +0300

    Add waves icon

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -1,6 +1,8 @@
 import { ReactComponent as AccountSvg } from './images/account.svg';
 import { ReactComponent as MenuSvg } from './images/menu.svg';
+import { ReactComponent as WavesSvg } from './images/waves.svg';
 import React from 'react';
 
 export const AccountIcon = (): JSX.Element => <AccountSvg className="icon-account"/>;
 export const MenuIcon = (): JSX.Element => <MenuSvg className="icon-menu"/>;
+export const WavesIcon = (): JSX.Element => <WavesSvg className="icon-waves"/>;
