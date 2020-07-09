9facc54571cbfdf65000578b88c2fa47f278fb82
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-07-09 21:09:17 +0300

    Add Icons component

---
+++ src/components/icons/Icons.tsx
@@ -0,0 +1,4 @@
+import { ReactComponent as AccountSvg } from './images/account.svg';
+import React from 'react';
+
+export const AccountIcon = (): JSX.Element => <AccountSvg className="icon-account"/>;
