c5e769daf5772fda74d2df0cd2b149f256909793
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-07-12 13:53:29 +0300

    Update navigation constants

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,4 +1,11 @@
 export const MAIN_ROUTE = '/';
+
 export const SIGN_UP_ROUTE = '/signup';
 export const LOGIN_ROUTE = '/login';
 export const LOGOUT_ROUTE = '/logout';
+
+export const PRICES_ROUTE = '/prices';
+export const SCHEDULE_ROUTE = '/schedule';
+export const ROOMS_ROUTE = '/rooms';
+export const CONTACTS_ROUTE = '/contacts';
+export const ABOUT_ROUTE = '/about';
